6ddd5bccac30c1b163f54e29975f517501e308ba
LoLoSenPai <loic.dlugosz@gmail.com>
2023-12-10 17:38:40 +0100

    Commented out code for loading modals

--- Client/Handlers/modals.js
+++ Client/Handlers/modals.js
@@ -1,18 +1,18 @@
-module.exports = (client, fs) => {
+// module.exports = (client, fs) => {
 
-    const modals = fs.readdirSync('./Client/Modals');
-    for (let category of modals) {
+//     const modals = fs.readdirSync('./Client/Modals');
+//     for (let category of modals) {
 
-        const files = fs.readdirSync(`./Client/Modals/${category}`);
-        for (let file of files) {
+//         const files = fs.readdirSync(`./Client/Modals/${category}`);
+//         for (let file of files) {
 
-            const inside_file = require(`../Modals/${category}/${file}`);
+//             const inside_file = require(`../Modals/${category}/${file}`);
 
-            if (inside_file.id) {
-                client.modals.set(inside_file.id, inside_file);
-            }
-        }
-    }
+//             if (inside_file.id) {
+//                 client.modals.set(inside_file.id, inside_file);
+//             }
+//         }
+//     }
 
-    console.log(`Loaded ${client.modals.size} modals!`);
-}
+//     console.log(`Loaded ${client.modals.size} modals!`);
+// }
